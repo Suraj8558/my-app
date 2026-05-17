03c685ae88aa0b70773ec5829e3e6b298b7e25e2
suraj <surajsk8558@gmail.com>
2026-05-17 11:34:19 +0530

    implement closures

--- public/javascript.js
+++ public/javascript.js
@@ -1,39 +1,3 @@
-// function abc () {
-//   var a = 10;
-
-//   function asd () {
-//     console.log("a", a);
-//   }
-//   asd()
-// }
-
-// abc()
-
-// let a = 10;
-// console.log(a);
-
-// const b = 100;
-// console.log(b);
-
-// {
-
-//   var a = 10;
-//   let ab = 20
-//   const abc = 30
-// }
-
-
-// console.log("a", a);
-// console.log("abc", abc);
-// console.log("ab", ab);
-
-
-
-
-
-
-
-
 function abc() {
  for (var i = 1; i < 5 ; i++ ) {
   function abcd (i) {
