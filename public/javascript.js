65b275034717eac5a61259e783db7d10bd675184
suraj <surajsk8558@gmail.com>
2026-05-23 15:20:37 +0530

    feat: implement custom map method and demonstrate higher-order functions for area calculation

--- public/javascript.js
+++ public/javascript.js
@@ -33,6 +33,53 @@ function sun(count) {
 
 sun(count)
 
+// Higher order funcction
+
+const arrData = [5,6,10,5,6];
+
+function area (arr) {
+  const result = [];
+   for(let i= 0; i <= arr.length - 1; i++) {
+    result.push(Math.PI * arr[i] *  arr[i])
+   }
+   return result;
+   
+}
+
+console.log("Area of the Data", area(arrData))
 
 
+function Area (arr) {
+  if(!arr) return ;
+  return Math.PI * arr * arr;
+}
+
+
+
+function CalculateArea (arr , CalculateArea) {
+  const result = [];
+   for(let i= 0; i <= arr.length - 1; i++) {
+    result.push(CalculateArea(arr[i]))
+   }
+   return result;
+   
+}
+console.log("Area of the CalculateArea", CalculateArea(arrData, Area))
+
+console.log("Area of the arrData", arrData.map(Area))
+
+Array.prototype.customMap = function (callback) {
+  if(!callback) return 
+  const result = [];
+   for(let i= 0; i <= this.length - 1; i++) {
+    result.push(callback(this[i]))
+   }
+   return result;
+   
+}
+
+
+
+
+// 
 
