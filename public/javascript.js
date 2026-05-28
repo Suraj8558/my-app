fa7e565c5cff9e289b0dfb7599f9dc360aea0189
suraj <surajsk8558@gmail.com>
2026-05-28 21:23:04 +0530

    feat: add product data structures and implement reduce methods for grouping and price calculation

--- public/javascript.js
+++ public/javascript.js
@@ -81,5 +81,51 @@ Array.prototype.customMap = function (callback) {
 
 
 
-// 
+// reducers  and filter
 
+
+const products = [
+  { id: 1, name: "Wireless Mouse", category: "Electronics", price: 29, inStock: true },
+  { id: 2, name: "Yoga Mat", category: "Fitness", price: 45.00, inStock: false },
+  { id: 3, name: "Mechanical Keyboard", category: "Electronics", price: 29, inStock: true },
+  { id: 4, name: "Running Shoes", category: "Fitness", price: 120.00, inStock: true },
+  { id: 5, name: "Coffee Maker", category: "Kitchen", price: 50, inStock: true },
+  { id: 6, name: "Blender", category: "Kitchen", price: 50, inStock: false },
+  { id: 7, name: "Running Shoes", category: "Fitness", price: 600.00, inStock: true },
+
+];
+
+const groupedByCategory = products.reduce((acc, product) => {
+  const category = product.category;
+  
+  // If the category doesn't exist yet in our object, initialize it as an empty array
+  if (!acc[category]) {
+    acc[category] = [];
+  }
+  
+  // Push the product into its respective category array
+  acc[category].push(product);
+  
+  return acc;
+}, {}); // Starting value is an empty object
+
+console.log("groupedByCategory", groupedByCategory);
+
+
+
+const highestprice = products.reduce((acc, product) => {
+  if (product.price >  acc.price ) {
+   return product.price
+  }
+  return acc 
+
+}, products[0])
+
+console.log("highestprice", highestprice);
+
+
+// const result = products.filter((item) => {
+//   return item.category  == "Kitchen" ;
+// }).map((x) => (x.name))
+
+// console.log("result", resultyarn dev
